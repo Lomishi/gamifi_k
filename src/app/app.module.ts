2dc36c6e99325552858d4806aa17a204590fadd4
JorgeIs-01 <infantessorolla@gmail.com>
2021-01-27 15:55:20 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+// import { InicioComponent } from './inicio/inicio.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { LoginProfesorComponent } from './login-profesor/login-profesor.component';
+import { RegistroAlumnoComponent } from './registro-alumno/registro-alumno.component';
+import { RegistroProfesorComponent } from './registro-profesor/registro-profesor.component';
+import { PerfilAlumnoComponent } from './perfil-alumno/perfil-alumno.component';
+import { PerfilProfesorComponent } from './perfil-profesor/perfil-profesor.component';
+import { MatButtonModule } from '@angular/material/button';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginProfesorComponent,
+    RegistroAlumnoComponent,
+    RegistroProfesorComponent,
+    PerfilAlumnoComponent,
+    PerfilProfesorComponent,
+
+    // InicioComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatButtonModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
