c13205a7b6284ed8826e7156d0bfc676e4b0559b
mcanela30 <marticanela2015@gmail.com>
2021-02-05 15:28:00 +0100

    04.2.2021

---
+++ src/app/model.ts
@@ -0,0 +1,9 @@
+export class registro {
+  constructor(
+    public nick : string,
+    public email : string,
+    public pwd : string,
+    public nombre : string,
+    public apellidos : string,
+  ){}
+}
