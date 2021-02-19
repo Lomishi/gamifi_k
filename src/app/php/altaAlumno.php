a2d688d20f9f6bbf7a44978456dba9fce1a1c905
mcanela30 <marticanela2015@gmail.com>
2021-02-19 15:51:44 +0100

    realizamos conexion a la bd

--- src/app/php/altaAlumno.php
+++ src/app/php/altaAlumno.php
@@ -1,8 +1,10 @@
 <?php
+require_once 'database.php';
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$data=file_get_contents("php://input");
 $jsonAlumno=json_decode(file_get_contents("php://input"));
 
 if(!$jsonAlumno){
