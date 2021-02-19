0e0e6f747e1b55dd1cb0177e31d34a8c6cdf519b
Juanmalc01 <juanmalozanochacot@gmail.com>
2021-02-19 18:56:13 +0100

    perfil php

--- src/app/components/registro/registro-profesor/registro-profesor.component.ts
+++ src/app/components/registro/registro-profesor/registro-profesor.component.ts
@@ -15,33 +15,20 @@ import{Router} from'@angular/router';
 
 export class RegistroProfesorComponent implements OnInit {
 
-  constructor(private ProfesorService: ProfesorService,
-    private snackbar: MatSnackBar,
-    private router:Router,) { }
+  constructor(private ProfesorService: ProfesorService
+    ) { }
 
   ngOnInit() {
 
   }
   profesorModel= new registro_profesor("","","","","","")
 
-  onSubmit(){
-    this.ProfesorService.addprofesor(this.profesorModel).subscribe(
-      (value:any) => {
-        console.log("respuesta de el servidor"+value);
 
-        this.snackbar.open('profesor guardado',undefined,{
-          duration:1500,
-        });
-        this.router.navigate(['/profesor']);
-      },
-      (error: any) => {
-        console.log(error);
-      }
+  alta() {
+    this.ProfesorService.addprofesor(this.profesorModel).subscribe(datos => {
 
-    )
-
-
-
+    });
   }
+
 
 }
---
+++ src/app/php/perfilAlumno.php
@@ -0,0 +1,63 @@
+<?php
+require_once 'database.php';
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$data=file_get_contents("php://input");
+$jsonAlumno = json_decode($data);
+
+
+
+
+
+if(!$jsonAlumno){
+  exit("No hay datos");
+
+}
+
+
+///////////////////////////////////////////////
+
+
+
+
+
+
+$logueado = 0;
+
+header("Content-Type: text/html;charset=utf-8");
+
+$instruccion = "select count(*) as cuantos from registro_alumno where nick = '$jsonProfesor->nick'";
+$resultado = mysqli_query($con, $instruccion);
+while ($fila = $resultado->fetch_assoc()) {
+	$numero = $fila["cuantos"];
+}
+if ($numero == 0) {
+	header("Location: login.html?alert=El usuario no existe");
+} else {
+	$instruccion = "select pwd as cuantos , id from registro_alumno where nick = '$jsonProfesor->nick'";
+	$resultado = mysqli_query($con, $instruccion);
+
+	while ($fila = $resultado->fetch_assoc()) {
+		$password2 = $fila["cuantos"];
+		$id = $fila["id"];
+	}
+
+	// Si la contraseña es incorrecta
+	if (!password_verify($password, $password2)) {
+		header("Location: login.html?alert=Contraseña incorrecta");
+
+	// Si la contraseña es correcta
+	} else {
+		echo "Login OK";
+		 $nick;
+		 $id;
+
+
+
+
+	}
+}
+?>
+
